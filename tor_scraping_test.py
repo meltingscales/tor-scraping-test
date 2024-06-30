6e87d8075e2ed5f07275819e89071d9ac11f1c11
Henry Post <HenryFBP@gmail.com>
2024-06-29 22:26:53 -0500

    show only onion links

--- tor_scraping_test.py
+++ tor_scraping_test.py
@@ -1,4 +1,5 @@
 from typing import List
+from urllib.parse import urlparse
 
 import requests
 from bs4 import BeautifulSoup
@@ -13,8 +14,15 @@ proxies = {
 url = 'http://lockbit7z2jwcskxpbokpemdxmltipntwlkmidcll2qirbu7ykg46eyd.onion'
 
 
+def is_url_onion(url: str) -> bool:
+    # extract domain from URL
+    parsed = urlparse(url)
+
+    return parsed.netloc.endswith('.onion')
+
+
 # Function to get all links from the onion URL
-def get_links_from_onion(url: str) -> List[str]:
+def get_all_links_from_onion(url: str) -> List[str]:
     try:
         response = requests.get(url, proxies=proxies)
         if response.status_code == 200:
@@ -29,7 +37,16 @@ def get_links_from_onion(url: str) -> List[str]:
         return []
 
 
-# Fetch and print links
-links = get_links_from_onion(url)
-for link in links:
-    print(link)
+if __name__ == '__main__':
+    # Fetch and print links
+    links = get_all_links_from_onion(url)
+
+    print(f"Links from {url}:")
+    for link in links:
+        print(link)
+
+    # fetch and print only onion links
+    onion_links = [link for link in links if is_url_onion(link)]
+    print(f"\nOnion links from {url}:")
+    for onion_link in onion_links:
+        print(onion_link)
